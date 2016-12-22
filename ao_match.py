f37562cd252a550c1f65b54756e42e115e3df04a
eeghor <eeghor@gmail.com>
2016-12-23 01:49:36 +1100

    shorter ids

--- ao_match.py
+++ ao_match.py
@@ -5,14 +5,15 @@ import jellyfish
 # command line arguments
 official_ao_data_file = sys.argv[1]
 oddsportal_ao_data_file = sys.argv[2]
+flashscore_ao_data_file = sys.argv[3]
 
 ao_df = pd.read_csv(official_ao_data_file, sep="\t") 
 op_df = pd.read_csv(oddsportal_ao_data_file, sep="\t") 
+fs_df = pd.read_csv(flashscore_ao_data_file, sep="\t") 
 
 nrows_ao = len(ao_df.index)
 nrows_op = len(op_df.index)
-
-# print("have {} match records from the AO web site and {} from OddsPortal...".format(nrows_ao, nrows_op))
+nrows_fs = len(fs_df.index)
 
 """
  how do we decide that it's the same match?
@@ -23,58 +24,87 @@ nrows_op = len(op_df.index)
 def get_ao_surname(st):
 
 	# IN: a string like "Rafael Nadal"
+	su = st[st.index(".") - 1] + st.split()[1][:2]
 
-	name_split = [w.strip().lower() for w in st.split()]   # ["Rafael", "Nadal"]
+	# ignore all parts with the full stop
+	# name_split = [w.strip().lower() for w in st.split() if "." not in w]   # ["Rafael", "Nadal"]
 
 	
-	if len(name_split) == 1:
-		su = name_split[0]
-	elif len(name_split) == 2:  # just name and surname
-		su = name_split[1]
-	else:
-		if "del" in name_split:
-			su = "_".join(name_split[name_split.index("del"):])
-		elif "de" in name_split:
-			su = "_".join(name_split[name_split.index("de"):])
-		elif len(name_split) == 4:
-			su = " ".join(name_split[-2:])
-		else:
-			su = name_split[-1]
+	# if len(name_split) == 1:
+	# 	su = name_split[0][0]  # e.g. John -> j
+	# else:  # John Watson -> jwa
+	# 	su = name_split[0][0] + name_split[1][:2]
+	# else:
+	# 	if "del" in name_split:
+	# 		su = "_".join(name_split[name_split.index("del"):])
+	# 	elif "de" in name_split:
+	# 		su = "_".join(name_split[name_split.index("de"):])
+	# 	elif len(name_split) == 4:
+	# 		su = " ".join(name_split[-2:])
+	# 	else:
+	# 		su = name_split[-1]
 
-	su = su.replace("-","_")
-	
-	return su
+	#su = su.replace("-","_")
+	# if "-" in su:
+	# 	su = su.split("-")[0]
+
+	return su.lower()
 
 def get_op_surname(st):
 
-	su = "_".join([w.strip().lower() for w in st.split() if "." not in w])
+	su = st[st.index(".") - 1] + st.split()[0][:2]
 
-	su = su.replace("-","_")
+	# su = "_".join([w.strip().lower() for w in st.split() if "." not in w])
+
+	# if "-" in su:
+	# 	su = su.split("-")[0]
 	
-	return su
+	return su.lower()
 
+def normalize_name(st):
 
+	st_split = st.split()
 
-# assume that surname is all but the very first word; special case: if there's del before the last word, then it's a part of surname
-plist1 = ao_df.player1.apply(lambda _: [get_ao_surname(_)] if "/" not in _ else [get_ao_surname(y) for y in _.split("/")])
-plist2 = ao_df.player2.apply(lambda _: [get_ao_surname(_)] if "/" not in _ else [get_ao_surname(y) for y in _.split("/")])
-ao_df["id"] = pd.Series([y for y in map(lambda x: "_".join(x), map(sorted, [x[0]+x[1] for x in zip(plist1, plist2)]))]).values
+	if "." not in st_split[0]:
+		nn = st_split[0][0] + "." + " " + st_split[1]
+	else:
+		nn = st_split[0] + " " + st_split[1]
+
+	return nn
 
 # date is either just a year or something like 2016-01-25
 ao_df["year"] = ao_df.date.apply(lambda _: _ if "-" not in _ else _.split("-")[0]).astype(int)
 
-# assume that everything except the bit with a dot is surname, e.g. de voest l.
-plist1 = op_df.player1.apply(lambda _: [get_op_surname(_)] if "/" not in _ else [get_op_surname(y) for y in _.split("/")])
-plist2 = op_df.player2.apply(lambda _: [get_op_surname(_)] if "/" not in _ else [get_op_surname(y) for y in _.split("/")])
-op_df["id"] = pd.Series([y for y in map(lambda x: "_".join(x), map(sorted, [x[0]+x[1] for x in zip(plist1, plist2)]))]).values
+ao_df.player1 = ao_df.player1.apply(lambda _: normalize_name(_) if "/" not in _ else "/".join([normalize_name(y) for y in _.split("/")]))
+ao_df.player2 = ao_df.player2.apply(lambda _: normalize_name(_) if "/" not in _ else "/".join([normalize_name(y) for y in _.split("/")]))
+# ao_df["player1"] = ao_df["player1"].apply(lambda _:  y.split()[0][0]+"."+" "+y for y in _.split("/") )
+#where_2016 = (ao_df["year"] == 2016)
+#where_not_2016 = ~ where_2016
+# assume that surname is all but the very first word; special case: if there's del before the last word, then it's a part of surname
+plist1 = ao_df.player1.apply(lambda _: [get_ao_surname(_)] if "/" not in _ else [get_ao_surname(y) for y in _.split("/")])
+plist2 = ao_df.player2.apply(lambda _: [get_ao_surname(_)] if "/" not in _ else [get_ao_surname(y) for y in _.split("/")])
 
-# # here dates are like 14 Jan 2009
+# plist1b = ao_df.player1.apply(lambda _: [get_op_surname(_)] if "/" not in _ else [get_op_surname(y) for y in _.split("/")])
+# plist2b = ao_df.player2.apply(lambda _: [get_op_surname(_)] if "/" not in _ else [get_op_surname(y) for y in _.split("/")])
+
+
+ao_df["id"] = pd.Series([y for y in map(lambda x: "_".join(x), map(sorted, [x[0]+x[1] for x in zip(plist1, plist2)]))]).values
+
+
+# assume that everything except the bit with a dot is surname, e.g. de voest l.
+plist1_op = op_df.player1.apply(lambda _: [get_op_surname(_)] if "/" not in _ else [get_op_surname(y) for y in _.split("/")])
+plist2_op = op_df.player2.apply(lambda _: [get_op_surname(_)] if "/" not in _ else [get_op_surname(y) for y in _.split("/")])
+op_df["id"] = pd.Series([y for y in map(lambda x: "_".join(x), map(sorted, [x[0]+x[1] for x in zip(plist1_op, plist2_op)]))]).values
+
+# here dates are like 14 Jan 2009
 op_df["year"] = op_df.date.apply(lambda _: _.split()[-1]).astype(int)
+
+fs_df["year"] = fs_df.date.apply(lambda _: _.split("-")[0]).astype(int)
+
+# op_df = op_df.append(fs_df, ignore_index=True)
 
 min_year_op = op_df.year.min()
 max_year_op = op_df.year.max()
-
-# # print("actual match starting times are available for years {} to {}".format(min_year_op, max_year_op))
 
 starting_times = []
 exact_match_idx = []
@@ -85,21 +115,6 @@ mult_match_idx = []
 nomatch_file = "nomatch.csv"
 multmatch_file = "multmatch.csv"
 
-# def match_again(surname1_ao, surname2_ao, year_ao):
-
-# 	# case 1: single
-# 	if ("/" not in surname1_ao) and ("/" not in surname2_ao):
-# 		singles_idx = ~ (op_df.sur_p1.apply(lambda _: "/" in _) & op_df.sur_p2.apply(lambda _: "/" in _))
-		
-# 		ii = op_df.year[singles_idx].isin([year_ao]) & (op_df.sur_p1[singles_idx].apply(lambda _: jellyfish.levenshtein_distance(surname1_ao, _) < 3) or 
-# 				op_df.sur_p2[singles_idx].apply(lambda _: jellyfish.levenshtein_distance(surname1_ao, _) < 3)) & 
-# 				([singles_idx][singles_idx].apply(lambda _: jellyfish.levenshtein_distance(surname2_ao, _) < 3) or 
-# 				op_df.sur_p2[singles_idx].apply(lambda _: jellyfish.levenshtein_distance(surname2_ao, _) < 3)) 
-
-# 		if sum(ii) == 1:
-# 			print("matched: ",surname1_ao,"vs", surname2_ao,"to", op_df.sur_p1[ii],"vs",op_df.sur_p2[ii])
-
-
 for i in range(nrows_ao):
 
 	print("matching {} vs {} played in {}...".format(plist1[i], plist2[i],ao_df.year[i]), end="")
@@ -109,22 +124,12 @@ for i in range(nrows_ao):
 		starting_times.append("N/A")
 		nomatch_absent_year_idx.append(i)
 		continue
-
-	# right_one = (
-	# 	op_df.year.isin([ao_df.year[i]]) & 
-	# 	op_df.sur_p1.apply(lambda _: len(_ & ao_df.sur_p1[i]) == len(_) or len(_ & ao_df.sur_p2[i]) == len(_)) & 
-	# 	op_df.sur_p2.apply(lambda _: len(_ & ao_df.sur_p1[i]) == len(_) or len(_ & ao_df.sur_p2[i]) == len(_)) )
 	
-	
-	mindex = op_df.year.isin([ao_df.year[i]]) & op_df.id.apply(lambda _: jellyfish.levenshtein_distance(ao_df.id[i], _) < 3)
-
-	# right_one = (
-	# 	op_df.year.isin([ao_df.year[i]]) & 
-	# 	op_df.sur_p1.apply(  lambda _: (jellyfish.levenshtein_distance(_ ,ao_df["id"][i]) < 3) or 
-	# 									(jellyfish.levenshtein_distance(_ ,ao_df.sur_p2[i][0]) < 3)  ) & 
-	# 	op_df.sur_p2.apply(  lambda _: jellyfish.levenshtein_distance(_ ,ao_df.sur_p1[i][0]) < 3 or 
-	# 										jellyfish.levenshtein_distance(_ ,ao_df.sur_p2[i][0]) < 3  ) )
-	
+	eindex = (op_df.year == ao_df.year[i]) & (op_df.id == ao_df.id[i])
+	if sum(eindex) == 1:
+		mindex = eindex
+	else:
+		mindex = op_df.year.isin([ao_df.year[i]]) & op_df.id.apply(lambda _: jellyfish.levenshtein_distance(ao_df.id[i], _) < 2)
 
 	NMTCH = sum(mindex)
 
